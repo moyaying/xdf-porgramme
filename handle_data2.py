e149c77235dbde17fdbe0c587138fa311ba3639e
moo <moyaying@163.com>
2021-06-13 16:44:34 +0800

    更新数据处理方式

--- handle_data2.py
+++ handle_data2.py
@@ -51,7 +51,7 @@ def add_virtual(key_name, item_value):
     return zones[:-1]
 
 
-def handle_cell(line_n, row_n, data):
+def handle_cell(row_n, data):
     if row_n == 0:  # 学员号
         pass  # ignore
     elif row_n == 1:  # 班级编码
@@ -67,10 +67,11 @@ def handle_cell(line_n, row_n, data):
     elif row_n == 5:  # 离班日期
         pass
     elif row_n == 6:  # 离班类型
-        if data == "无":
-            return [0]
-        else:
-            return [1]
+        # if data == "无":
+        #     return [0]
+        # else:
+        #     return [1]
+        pass
     elif row_n == 7:  # 班级标价
         return [data]
     elif row_n == 8:  # 实缴金额
@@ -78,9 +79,10 @@ def handle_cell(line_n, row_n, data):
     elif row_n == 9:  # 平均实缴金额
         return [data]
     elif row_n == 10:  # 标准部门
-        key = "标准部门"
-        idx = add_enum(key, ['少儿部'], 1, data)
-        return [idx]
+        # key = "标准部门"
+        # idx = add_enum(key, ['少儿部'], 1, data)
+        # return [idx]
+        pass
     elif row_n == 11:  # 年级
         if data == "二年级":
             return [2]
@@ -101,30 +103,34 @@ def handle_cell(line_n, row_n, data):
         else:
             return [1]
     elif row_n == 12:  # 科目
-        # key = "科目"
-        # idx = add_enum(key, ['语文', '数学'], 1, data)
-        # return [idx]
-        v = add_virtual("科目", data)  # 使用虚拟变量
-        print(v, data)
-        return v
+        key = "科目"
+        idx = add_enum(key, ['语文', '数学'], 1, data)
+        return [idx]
+        # v = add_virtual("科目", data)  # 使用虚拟变量
+        # print(v, data)
+        # return v
     elif row_n == 13 or row_n == 14:  # 开课日期, 结课日期.  格式支持: xxxx-xx-xx, xxxx/xx/xx
-        obj = re.search(r'(\d{4}).(\d{2}).(\d{2})', str(data), re.M | re.I)
-        values = obj.groups()
-        return [values[0], int(values[1]), int(values[2])]
+        # obj = re.search(r'(\d{4}).(\d{2}).(\d{2})', str(data), re.M | re.I)
+        # values = obj.groups()
+        # return [values[0], int(values[1]), int(values[2])]
+        pass
     elif row_n == 15:  # 教学区, 虚拟
-        return add_virtual("教学区", data)
+        # return add_virtual("教学区", data)
+        pass
     elif row_n == 16:  # 班级归属
-        obj = re.search(r'\D*(\d+).*', str(data), re.M | re.I)
-        values = obj.groups()
-        return [values[0]]
+        # obj = re.search(r'\D*(\d+).*', str(data), re.M | re.I)
+        # values = obj.groups()
+        # return [values[0]]
+        pass
     elif row_n == 17:  # 离班课次
         return [data]
     elif row_n == 18:  # 课次
         return [data]
     elif row_n == 19:  # 授课方式
-        key = "授课方式"
-        idx = add_enum(key, ['线上'], 1, data)
-        return [idx]
+        # key = "授课方式"
+        # idx = add_enum(key, ['线上'], 1, data)
+        # return [idx]
+        pass
     elif row_n == 20:  # 在读学校
         pass
     elif row_n == 21:  # 财年
@@ -139,9 +145,10 @@ def handle_cell(line_n, row_n, data):
     elif row_n == 24:  # 学员编号+科目
         pass
     elif row_n == 25:  # 主代课教师
-        key = "主代课教师"
-        idx = add_enum(key, None, 1, data)
-        return [idx]
+        # key = "主代课教师"
+        # idx = add_enum(key, None, 1, data)
+        # return [idx]
+        pass
     elif row_n == 26:  # 是否管理者
         if data == '管理者':
             return [1]
@@ -149,25 +156,28 @@ def handle_cell(line_n, row_n, data):
     elif row_n == 27:  # 教师教龄（月）
         return [data]
     elif row_n == 28:  # 教师性别
-        if data == "男":
+        if data == "女":
             return [1]
         return [0]
     elif row_n == 29:  # 所学专业
-        key = "所学专业"
-        idx = add_enum(key, None, 1, data)
-        return [idx]
+        # key = "所学专业"
+        # idx = add_enum(key, None, 1, data)
+        # return [idx]
+        pass
     elif row_n == 30:  # 毕业院校
-        key = "毕业院校"
-        idx = add_enum(key, None, 1, data)
-        return [idx]
+        # key = "毕业院校"
+        # idx = add_enum(key, None, 1, data)
+        # return [idx]
+        pass
     elif row_n == 31:  # 是否是四类人才
         if data == "是":
             return [1]
         return [0]
     elif row_n == 32:  # 岗位名称
-        key = "岗位名称"
-        idx = add_enum(key, None, 1, data)
-        return [idx]
+        # key = "岗位名称"
+        # idx = add_enum(key, None, 1, data)
+        # return [idx]
+        pass
     elif row_n == 33:  # 学历
         key = "学历"
         idx = add_enum(key, ['大专', '本科', '硕士研究生', '博士研究生'], 1, data)
@@ -197,26 +207,30 @@ for rows in ws.rows:
         titles = []
         for row_num in range(0, len(rows)):
             title = rows[row_num].value
-            if row_num in [0, 1, 2, 4, 5, 20, 21, 24]:  # ignore
+            if row_num in [0, 1, 2, 4, 5, 6, 10, 13, 14, 15, 16, 19, 20, 21, 24, 25, 29, 30, 32]:  # ignore
+                # print('ignore', title)
                 pass
-            elif row_num == 12:  # 科目，使用虚拟变量
-                titles.extend(new_virtual("科目", 3))
-            elif row_num == 13 or row_num == 14:  # 开课日期, 结课日期
-                titles.extend([title + '-年', title + '-月', title + '-日'])
-            elif row_num == 15:  # 教学区, 虚拟
-                titles.extend(new_virtual("教学区", 10))
+            # elif row_num == 12:  # 科目，使用虚拟变量
+            #     titles.extend(new_virtual("科目", 3))
+            # elif row_num == 13 or row_num == 14:  # 开课日期, 结课日期
+            #     titles.extend([title + '-年', title + '-月', title + '-日'])
+            # elif row_num == 15:  # 教学区, 虚拟
+            #     titles.extend(new_virtual("教学区", 10))
             else:
                 titles.append(title)
+        print(titles, len(titles))
         csv_col.writerow(titles)
     else:  # 处理row数据
         row_data = []
         for row_num in range(0, len(rows)):
             item = rows[row_num].value
-            if row_num == 0 and item is None:  # 学员号是 None => 空行
+            if row_num == 0 and item is None:  # 学员号是 None 则 当作空行
                 break
-            cell_data = handle_cell(line_num, row_num, item)
+            cell_data = handle_cell(row_num, item)
             row_data.extend(cell_data)
-        csv_col.writerow(row_data)
+        if len(row_data) > 0:
+            print(row_data, len(row_data), line_num)
+            csv_col.writerow(row_data)
     line_num = line_num + 1
 
 # 写titles
